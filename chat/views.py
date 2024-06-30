c44d954d09b174fdf251eaaf67b81dded09cd9a6
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2024-06-30 10:11:10 +0500

    save change
    save change

--- chat/views.py
+++ chat/views.py
@@ -7,3 +7,18 @@ def lobby(request):
         print('post')
     print(request.POST.get('message'))
     return render(request, 'chat/lobby.html')
+
+from django.http import HttpResponse
+from django.singlefile import SingleFileApp
+
+app = SingleFileApp()
+
+
+@app.path("")
+def index(request):
+    name = request.GET.get("name", "World")
+    return HttpResponse(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    app.main()
